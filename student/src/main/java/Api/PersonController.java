7fc168064e4147f61835872c722481b27cf32971
szautner.b <szautner.b@gmail.com>
2023-10-20 13:41:29 +0000

    GetMapping

--- student/src/main/java/Api/PersonController.java
+++ student/src/main/java/Api/PersonController.java
@@ -4,6 +4,7 @@ package Api;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,7 +24,7 @@ public class PersonController {
 @PostMapping
 public void insertPerson(@RequestBody Person person){
     personService.addPerson(person);
-}
+}@GetMapping
     public List<Person> getAllPeople(){
     return personService.getAllPeople();
 }
