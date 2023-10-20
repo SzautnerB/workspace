d7a958056df4feea8dc9638f7ca046aa6cf43468
szautner.b <szautner.b@gmail.com>
2023-10-20 13:25:05 +0000

    Requestmapping

--- student/src/main/java/Api/PersonController.java
+++ student/src/main/java/Api/PersonController.java
@@ -2,10 +2,13 @@ package Api;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import model.Person;
 import service.PersonService;
+@RequestMapping("api/v1/person")
 @RestController
 public class PersonController {
     private final PersonService personService;
@@ -15,7 +18,7 @@ public class PersonController {
         this.personService=personService;
     }
 @PostMapping
-public void insertPerson(Person person){
+public void insertPerson(@RequestBody Person person){
     personService.addPerson(person);
 }
 }
